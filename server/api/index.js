8c8ee4d2946fc6ae3151e4e0039a52a6ff210449
Thomas Luo <tluo9713@gmail.com>
2019-05-13 10:28:27 -0400

    worked on candy model and routes

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,7 @@
-'use strict'
+'use strict';
 
-const router = require('express').Router()
+const router = require('express').Router();
+const Candy = require('../db/models/Candy');
 
 // Your routes go here!
 // NOTE: Any routes that you put here are ALREADY mounted on `/api`
@@ -18,10 +19,37 @@ const router = require('express').Router()
 // but you DON'T have a corresponding router, this piece of
 // middleware will generate a 404, and send it to your
 // error-handling endware!
-router.use((req, res, next) => {
-  const err = new Error('API route not found!')
-  err.status = 404
-  next(err)
-})
 
-module.exports = router
+router.get('/candies', (req, res, next) => {
+  try {
+    const data = Candy.findAll();
+    res.send(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/candies/:name', (req, res, next) => {
+  const candyName = req.params.name;
+  try {
+    let data = Candy.findOne({
+      where: {
+        name: candyName,
+      },
+    });
+    res.send(data);
+  } catch (error) {
+    next(error);
+  }
+  //candy name = req.params.name
+  //find one where name === candy name
+  res.body();
+});
+
+router.use((req, res, next) => {
+  const err = new Error('API route not found!');
+  err.status = 404;
+  next(err);
+});
+
+module.exports = router;
